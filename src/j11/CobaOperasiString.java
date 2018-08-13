dab5d759284fd27b8ee6524086f60f109435dd58
RakhiDemsi <RakhiDemsi@LAPTOP-4VLPV026>
2018-08-13 18:32:37 +0700

    Initial commit

--- src/j11/CobaOperasiString.java
+++ src/j11/CobaOperasiString.java
@@ -1,8 +1,7 @@
 package j11;
-
 public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
-        System.out.println("Identitas: " + identitas);
+        System.out.println("Identitas: " + identitas);        
     }
 }
