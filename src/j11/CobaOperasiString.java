a864bb265a2efb9fe1d8aa4031cbd6d0e8c83f33
RakhiDemsi <RakhiDemsi@LAPTOP-4VLPV026>
2018-08-13 19:20:46 +0700

    Coba method length & isEmpty

--- src/j11/CobaOperasiString.java
+++ src/j11/CobaOperasiString.java
@@ -2,6 +2,16 @@ package j11;
 public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
-        System.out.println("Identitas: " + identitas);        
+        System.out.println("Identitas: " + identitas);
+        
+        String x = "Operasi";
+        System.out.println("Isi variabel x : " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("Isi Variabel y : " + y);
+        System.out.println("y adalah kosong : " + x.isEmpty());
     }
 }
