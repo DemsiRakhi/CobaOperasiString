a423e6c05902bf14e49d7f3cf3b44809ea66522b
DemsiRakhi <muhammad_zidan_27rpl@student.smktelkom-mlg.sch.id>
2018-08-14 12:59:53 +0700

    Initial Commit

---
+++ src/j11/ConvertVocal2Angka.java
@@ -0,0 +1,15 @@
+package j11;
+
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal ke Angka)\n");
+    }   
+    public static void main(String[] args) {
+        String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
+        tampilJudul(identitas);
+    }
+    
+    
+}
