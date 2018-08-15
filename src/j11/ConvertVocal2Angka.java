69cf3c238124fb059736c40564fdca6d3a1db40b
DemsiRakhi <muhammad_zidan_27rpl@student.smktelkom-mlg.sch.id>
2018-08-15 10:22:07 +0700

    Tambah Input

--- src/j11/ConvertVocal2Angka.java
+++ src/j11/ConvertVocal2Angka.java
@@ -1,15 +1,23 @@
 package j11;
-
+import java.util.Scanner;
 public class ConvertVocal2Angka {
     private static void tampilJudul(String identitas) {
         System.out.println("Identitas : " + identitas);
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal ke Angka)\n");
     }   
+    private static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli: " + kalimat);
+        
+        return kalimat;
+    }
     public static void main(String[] args) {
         String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
         tampilJudul(identitas);
-    }
-    
-    
+        String kalimat = tampilInput();
+    }        
 }
