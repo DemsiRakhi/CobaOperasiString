47c6c8418d2f385fd7e78721539123b9bf44b8b0
DemsiRakhi <muhammad_zidan_27rpl@student.smktelkom-mlg.sch.id>
2018-08-15 11:16:08 +0700

    Tambah Tampil Hasil

--- src/j11/ConvertVocal2Angka.java
+++ src/j11/ConvertVocal2Angka.java
@@ -25,10 +25,18 @@ public class ConvertVocal2Angka {
         
         return kalimat;
     }
+    private static void tampilPerKata(String kalimat, String convert) {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+        for(int i = 0; i < arrKal.length; i++)
+            System.out.println(arrKal[i] + " => " + arrCon[i]);
+    }
     public static void main(String[] args) {
         String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
         tampilJudul(identitas);
         String kalimat = tampilInput();
         String convert = vocal2Angka(kalimat);
+        tampilPerKata(kalimat, convert);
     }        
 }
